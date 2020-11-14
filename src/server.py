21e2855a3c0a60c67e30b4a58dc5cea2d02451c3
sjmulhern <sullivanmulhern@gmail.com>
2020-11-14 15:32:05 -0500

    Made bombast server only server bombast

--- src/server.py
+++ src/server.py
@@ -4,26 +4,16 @@ global app
 app = Flask(__name__)
 
 def index():
-    return render_template('index.html')
-
-
-def bombast():
     return render_template('bombast.html')
-
-
-def good_provider():
-    return render_template('goodProvider.html')
 
 
 def create_app():
     app.config["DEBUG"] = True
     app.add_url_rule("/", view_func=index)
-    app.add_url_rule("/bombast", view_func=bombast)
-    app.add_url_rule("/goodProvider", view_func=good_provider)
 
 
 if __name__ == "__main__":
     create_app()
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="localhost", port=80, debug=True)
 
 
